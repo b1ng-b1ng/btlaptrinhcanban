07688ca8c21d62bf5c0233a6ff4db9f8dda4152b
duda <rongthien093@gmail.com>
2025-04-18 15:18:04 +0700

    done

---
+++ input-output/bai3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b;
+    printf("nhap a=");
+    scanf("%d",&a);
+    printf("nhap b=");
+    scanf("%d",&b);
+    printf("tong a+b=%d\n",a+b);
+    printf("hieu a-b =%d\n",a-b);
+    printf("tich a*b = %d\n",a*b);
+    printf("thuong a/b= %.2lf\n",1.0*a/b);
+    return 0;
+}
---
+++ input-output/bai4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+    int main(){
+    double n;
+    printf("nhap vao ban kinh  R=");
+    scanf("%lf",&n);
+    float PI =3.14;
+    printf("Dien tich hinh cau la S=%.2lf\n",4.0*PI*n*n);
+    printf("The tich hinh cau la V=%.2lf",1.0*4/3*PI*n*n*n);
+    return 0;
+    }
